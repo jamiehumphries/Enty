7ddf43a918a5e709be77c48de9acef3d605b22c3
Jamie Humphries <jamie.humphries@softwire.com>
2015-02-21 21:39:00 +0000

    Add connection string provider for SQL Server Compact.

--- EntityFrameworkTestDb.NUnit.Tests/NUnitTestNameProviderTests.cs
+++ EntityFrameworkTestDb.NUnit.Tests/NUnitTestNameProviderTests.cs
@@ -5,10 +5,11 @@
 
     public class NUnitTestNameProviderTests
     {
+        private readonly NUnitTestNameProvider provider = new NUnitTestNameProvider();
+
         [Test]
         public void Current_test_name_is_returned_in_full()
         {
-            var provider = new NUnitTestNameProvider();
             provider.CurrentTestName.Should().Be("Current_test_name_is_returned_in_full");
         }
     }
---
+++ EntityFrameworkTestDb.SqlServerCompact/SqlServerCompactConnectionStringProvider.cs
@@ -0,0 +1,20 @@
+﻿namespace EntityFrameworkTestDb.SqlServerCompact
+{
+    using EntityFrameworkTestDb.Configuration;
+    using System;
+    using System.IO;
+
+    public class SqlServerCompactConnectionStringProvider : ITestDbConnectionStringProvider
+    {
+        public string GetConnectionString(string testName, DateTime executionTime)
+        {
+            var cleanedTestName = String.Concat(testName.Split(Path.GetInvalidFileNameChars()));
+            var dbFileName = cleanedTestName + executionTime.ToString("yyyyMMddHHmmssf");
+            if (dbFileName.Length > 100)
+            {
+                dbFileName = dbFileName.Substring(0, 50) + "…" + dbFileName.Substring(dbFileName.Length - 50);
+            }
+            return String.Format("Data Source={0}.sdf;", dbFileName);
+        }
+    }
+}
