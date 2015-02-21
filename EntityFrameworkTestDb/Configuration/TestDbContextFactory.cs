26f4966ffa3e86e617c61d5d96d52941e5507717
Jamie Humphries <jamie.humphries@softwire.com>
2015-02-21 21:56:29 +0000

    Add standard context factories.

---
+++ EntityFrameworkTestDb/Configuration/TestDbContextFactory.cs
@@ -0,0 +1,25 @@
+﻿namespace EntityFrameworkTestDb.Configuration
+{
+    using System;
+    using System.Data.Entity;
+
+    public class TestDbContextFactory<T> : TestDbContextFactory where T : DbContext
+    {
+        public TestDbContextFactory() : base(connectionString => (T)Activator.CreateInstance(typeof(T), connectionString)) {}
+    }
+
+    public class TestDbContextFactory : ITestDbContextFactory
+    {
+        private readonly Func<string, DbContext> dbContextFromConnectionString;
+
+        public TestDbContextFactory(Func<string, DbContext> dbContextFromConnectionString)
+        {
+            this.dbContextFromConnectionString = dbContextFromConnectionString;
+        }
+
+        public DbContext GetDbContext(string connectionString)
+        {
+            return dbContextFromConnectionString.Invoke(connectionString);
+        }
+    }
+}
