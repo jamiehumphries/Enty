aae2abe742b7b65a9c532c50ac83a848938fb592
Jamie Humphries <jamie.humphries@softwire.com>
2015-05-13 17:26:43 +0100

    Add thin implementations for all config interfaces.

---
+++ TestDb.Tests/Configuration/ConnectionStringProviderTests.cs
@@ -0,0 +1,36 @@
+﻿namespace EntityTestDb.Tests.Configuration
+{
+    using EntityTestDb.Configuration;
+    using FluentAssertions;
+    using global::NUnit.Framework;
+    using System;
+
+    public class ConnectionStringProviderTests
+    {
+        [Test]
+        public void Provides_static_connection_string_if_constructed_with_one()
+        {
+            // Given
+            var provider = new ConnectionStringProvider("dummy connection string");
+
+            // When
+            var connectionString = provider.GetConnectionString("Test_name", DateTime.Now);
+
+            // Then
+            connectionString.Should().Be("dummy connection string");
+        }
+
+        [Test]
+        public void Uses_delegate_from_constructor()
+        {
+            // Given
+            var provider = new ConnectionStringProvider((testName, executionDate) => testName + executionDate.ToString("yyyyMMdd"));
+
+            // When
+            var connectionString = provider.GetConnectionString("Test", new DateTime(1989, 05, 19));
+
+            // Then
+            connectionString.Should().Be("Test19890519");
+        }
+    }
+}
---
+++ TestDb/Configuration/ConnectionStringProvider.cs
@@ -0,0 +1,21 @@
+﻿namespace EntityTestDb.Configuration
+{
+    using System;
+
+    public class ConnectionStringProvider : IConnectionStringProvider
+    {
+        private readonly Func<string, DateTime, string> getConnectionString;
+
+        public ConnectionStringProvider(string connectionString) : this((testName, executionTime) => connectionString) {}
+
+        public ConnectionStringProvider(Func<string, DateTime, string> getConnectionString)
+        {
+            this.getConnectionString = getConnectionString;
+        }
+
+        public string GetConnectionString(string testName, DateTime executionTime)
+        {
+            return getConnectionString(testName, executionTime);
+        }
+    }
+}
